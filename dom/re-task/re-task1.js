5046ac07b3b80f3b7e28e2c57b2278316925b7a5
iam047 <iam047iam@gmail.com>
2018-02-16 16:20:29 +0200

    cleaning the code

--- dom/re-task/re-task1.js
+++ dom/re-task/re-task1.js
@@ -1,29 +1,14 @@
-/*
-function myFunction() {
-    var table = document.createElement("TABLE");
-    table.setAttribute("id", "myTable");
-    document.body.appendChild(table);
-
-    var y = document.createElement("TR");
-    y.setAttribute("id", "myTr");
-    document.getElementById("myTable").appendChild(y);
-
-    var z = document.createElement("TD");
-    document.getElementById("myTr").appendChild(z);
-}
-
-*/
 var row = prompt('Кількість столбців?');
 var cell = prompt('Кількість рядків?');
 ;(function() {
     var table = document.createElement('TABLE');
     table.setAttribute("id", "myTable");
-    var tblB = document.createElement('TBODY');
-    table.appendChild(tblB);
-    for( var i = 0; i< row; i++) {
+    var tbody = document.createElement('TBODY');
+    table.appendChild(tbody);
+    for( var i = 0; i < row; i++) {
         var tr = document.createElement('TR');
         tr.setAttribute("id", "myTR");
-          tblB.appendChild(tr);
+          tbody.appendChild(tr);
           for( var j = 0; j < cell; j++ ) {
               var td = document.createElement('TD');
               tr.appendChild(td);
@@ -32,46 +17,31 @@ var cell = prompt('Кількість рядків?');
     document.body.appendChild(table);
 }());
 
-
-function asd (e) {
+function addclass (e) {
      var event = e || window.event;
      var target = event.CurrentTarget || event.srcElement;
      if (target.tagName == 'TD') {
          target.className = "red";
      }
  };
-myTable.addEventListener('click', asd, false);
+myTable.addEventListener('click', addclass, false);
+
 window.onload = pageLoad;
 function pageLoad() {
     var startButton = document.getElementById("start");
-
     startButton.onclick = alertMe;
-}
-
-function alertMe() {
-    setInterval(function () {
-        x = document.getElementsByTagName('td');
-        console.log(x);
-        for(var i = 0; i < x.length; i++){
-            if (x[i].className === 'red') {
-                x[i].className = 'white';
-            } else {
-                x[i].className = 'red';
+    function alertMe() {
+        setInterval(function () {
+            x = document.getElementsByTagName('td');
+            console.log(x);
+            for(var i = 0; i < x.length; i++){
+                if (x[i].className === 'red') {
+                    x[i].className = 'white';
+                } else {
+                    x[i].className = 'red';
+                }
             }
-        }
-    }, 1000);
+        }, 1000);
+    }
 }
 
-
-function overtaking() {
-    x = document.getElementsByTagName('td');
-    console.log(x);
-      for(var i = 0; i < x.length; i++){
-        if (x[i].className === 'red') {
-            x[i].className = 'white';
-        } else {
-            x[i].className = 'red';
-        }
-    }
-
-};
