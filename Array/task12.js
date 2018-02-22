82b9b0c71f623e9329a6d3d1115e74796e50d74d
iam047 <iam047iam@gmail.com>
2018-02-22 11:49:24 +0200

    add

--- Array/task12.js
+++ Array/task12.js
@@ -1,10 +1,12 @@
-var arr = [1,4,5,6,7,21];
+var arr = [1,4,5];
 var sum = 0;
 var sumMnoj = 1;
 for (var i = 0; i < arr.length; i++) {
     sum += arr[i];
+    console.log(sum);
 }
 for (var i = 0; i < arr.length; i++) {
 sumMnoj *= arr[i];
+console.log(sumMnoj);
 }
 console.log(sum, sumMnoj);
