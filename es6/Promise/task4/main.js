612c1e601e1e8de2b2c89ee0138f24c7d80f7d80
iam047 <iam047iam@gmail.com>
2018-02-27 16:28:23 +0200

    new promise1

--- es6/Promise/task4/main.js
+++ es6/Promise/task4/main.js
@@ -1,47 +1,41 @@
-class MyPromi {
+class MyPromise {
     constructor(callback) {
-        this._state = 'panding';
+        this._state = 'pending';
         this.promise = {
             resolve: [],
             reject: []
         };
-        this.eror = null;
-
         const resolve = value => {
-           if(this._state !== 'pending') {
-               this._state = 'resolve';
-
-            let val = value;
-            try {
-                this.promise.resolve.forEach((fn) => {
-                    val = fn(val);
-                });
-            } catch (error) {
-                this.promise.reject = [];
-
+            if(this._state === 'pending') {
+                this._state = 'resolve';
+                let val = value;
+                try {
+                    this.promise.resolve.forEach((fn) => {
+                        val = fn(val);
+                    });
+                } catch (error) {
+                    this.promise.reject = [];
+                }
             }
-           }
         };
-
         const reject = (error) => {
-            if(this._state !== 'pending') {
+            if(this._state === 'pending') {
                 this._state = 'reject';
                 let er = error;
                 this.promise.reject.forEach((fn) => {
                     er = fn(er);
-            })
+                })
             }
         };
 
         callback(resolve, reject);
     }
 
-    then(resolve,rejetc) {
-        if(this._state = 'resolve'){
-            this.promise.resolve.push(resolve);
+    then(resolve, reject) {
+        if(this._state === 'pending'){
+           resolve && this.promise.resolve.push(resolve);
+           reject && this.promise.reject.push(reject);
             console.log(this.promise);
-        }else {
-            this.promise.reject.push(rejetc);
         }
 
         return this;
@@ -51,7 +45,6 @@ class MyPromi {
 function mul(val) {
     return val * val;
 }
-
-const my = new MyPromi(resolve => setTimeout(() => resolve(7),1000));
+const my = new MyPromise(resolve => setTimeout(() => resolve(7),1000));
 my.then(value => mul(value))
   .then(val=> console.log(val));
