26143d9ebef12aae57711094b61ec2f240eb9e84
iam047 <iam047iam@gmail.com>
2018-02-27 15:31:50 +0200

    new promise

--- es6/Promise/task4/main.js
+++ es6/Promise/task4/main.js
@@ -1,116 +1,57 @@
-function MyPromise(task) {
-    this._state = 'pending';
-    this._resolve_handler = 0;
-    this._reject_handler = 0;
-    this._resolved_value = 0;
-    this._rejected_value = 0;
-    this._resolve = this._resolve.bind(this);
-    this._reject = this._reject.bind(this);
-    task(this._resolve, this._reject);
-    return this;
-}
-
-MyPromise.resolved = function (val) {
-    const p = new MyPromise(res=>res(val));
-    p._state = 'resolved';
-    return p;
-};
-
-MyPromise.prototype = {
-    _resolve: function(val) {
-        this._state = 'resolved';
-        this._resolved_value  = val;
-        if (this._resolve_handler) {
-            this._resolve_handler(val);
-        }
-    },
-    _reject: function(err) {
-        this._state = 'rejected';
-        this._rejected_value = err;
-        if (this._reject_handler) {
-            this._reject_handler(err);
-        }
-    },
-    then: function(resolved, rejected) {
-        return new MyPromise((res, rej)=> {
-            if (this._state === 'pending') {
-                this._resolve_handler = function (val) {
-                    res(resolved(val));
-                };
-                this._reject_handler = function (err) {
-                    res(rejected(err));
-                }
-            } else if (this._state === 'resolved') {
-                res(resolved(this._resolved_value));
-            } else if (this._state === 'rejected') {
-                res(rejected(this._rejected_value));
-            }
-        });
-    }
-};
-
 class MyPromi {
-    constructor(task){
-        this._state = 'pending';
-        this._resolve_handler = 0;
-        this._reject_handler = 0;
-        this._resolved_value = 0;
-        this._rejected_value = 0;
-        this._resolve = this._resolve.bind(this);
-        this._reject = this._reject.bind(this);
-        task(this._resolve, this._reject);
+    constructor(callback) {
+        this._state = 'panding';
+        this.promise = {
+            resolve: [],
+            reject: []
+        };
+        this.eror = null;
+
+        const resolve = value => {
+           if(this._state !== 'pending') {
+               this._state = 'resolve';
+
+            let val = value;
+            try {
+                this.promise.resolve.forEach((fn) => {
+                    val = fn(val);
+                });
+            } catch (error) {
+                this.promise.reject = [];
+
+            }
+           }
+        };
+
+        const reject = (error) => {
+            if(this._state !== 'pending') {
+                this._state = 'reject';
+                let er = error;
+                this.promise.reject.forEach((fn) => {
+                    er = fn(er);
+            })
+            }
+        };
+
+        callback(resolve, reject);
+    }
+
+    then(resolve,rejetc) {
+        if(this._state = 'resolve'){
+            this.promise.resolve.push(resolve);
+            console.log(this.promise);
+        }else {
+            this.promise.reject.push(rejetc);
+        }
+
         return this;
     }
-
-    resolved (val) {
-        const p = new MyPromise(res=>res(val));
-        p._state = 'resolved';
-        return p;
-    };
-
-
-    _resolve(val) {
-        this._state = 'resolved';
-        this._resolved_value  = val;
-        if (this._resolve_handler) {
-            this._resolve_handler(val);
-        }
-    }
-    _reject(err) {
-        this._state = 'rejected';
-        this._rejected_value = err;
-        if (this._reject_handler) {
-            this._reject_handler(err);
-        }
-    }
-    then (resolved, rejected) {
-        return new MyPromise((res, rej)=> {
-            if (this._state === 'pending') {
-                this._resolve_handler = function (val) {
-                    res(resolved(val));
-                };
-                this._reject_handler = function (err) {
-                    res(rejected(err));
-                }
-            } else if (this._state === 'resolved') {
-                res(resolved(this._resolved_value));
-            } else if (this._state === 'rejected') {
-                res(rejected(this._rejected_value));
-            }
-        });
-    }
-
-
 }
 
-const myPromise = new MyPromise(resolve => setTimeout(() => resolve(123),5000));
-myPromise.then(
-    value => console.log('success', value),
-    error => console.log('error', error)
-);
+function mul(val) {
+    return val * val;
+}
 
-const my = new MyPromi(resolve => setTimeout(() => resolve(777),5000));
-my.then(
-    value => console.log('success', value),
-    error => console.log('error', error)
-);
+const my = new MyPromi(resolve => setTimeout(() => resolve(7),1000));
+my.then(value => mul(value))
+  .then(val=> console.log(val));
